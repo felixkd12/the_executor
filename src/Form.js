773eb0b6b97eefd27f8741475388c8c280c0ed27
felixkd12 <felixkd99@gmail.com>
2023-01-17 20:26:51 +0700

    Learn Form finish

--- src/Form.js
+++ src/Form.js
@@ -1,27 +1,35 @@
 import React from "react";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
 
 export const Form = () => {
 
-    const {register, handleSubmit} = useForm ()
-
     const schema = yup.object().shape({
-        fullName: yup.string().required(),
+        fullName: yup.string().required("Your fullname is required"),
         email: yup.string().email().required(),
-        age: yup.number().positive().integer.min(18).required(),
-        
-    })
+        age: yup.number().required(),
+        password: yup.string().min(4).max(20).required(),
+        confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required()
+
+    });
+
+    const { register, handleSubmit, formState: {errors} } = useForm({
+        resolver: yupResolver(schema), 
+    });
+
     const onSubmit = (data) => {
         console.log(data);
     }
+
     return (
-        <form onSubmit = {handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder="Full Name" {...register("fullname")} />
-            <input type="text" placeholder="Last Name" {...register("email")}/>
-            <input type="number" placeholder="Age"{...register("age")}/>
-            <input type="password" placeholder= "Password" {...register("password")}/>
-            <input type="password" placeholder= "Confirm Password" {...register("confirmPassword")}/>
+            <h1> {errors.fullName} </h1>
+            <input type="text" placeholder="example@gmail.com" {...register("email")} />
+            <input type="number" placeholder="Age"{...register("age")} />
+            <input type="password" placeholder="Password" {...register("password")} />
+            <input type="password" placeholder="Confirm Password" {...register("confirmPassword")} />
             <input type="submit" />
         </form>
 
